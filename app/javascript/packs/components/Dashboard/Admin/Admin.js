14a18b44ae7b1c8abb3327133c11e826f0d095e8
jallenmanaloto <jallen.manaloto@gmail.com>
2022-01-03 00:26:07 +0800

    Integrate context

--- app/javascript/packs/components/Dashboard/Admin/Admin.js
+++ app/javascript/packs/components/Dashboard/Admin/Admin.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import AdminContext from '../../Contexts/AdminContext'
 import Navbar from './Navbar'
-import Sidebar from './Sidebar'
+import Header from './Header'
+import Traders from './Traders'
+import TraderDetails from './TraderDetails'
+
 
 const Admin = () => {
+    const [adminDashboard, setAdminDashboard] = useState(true);
+    const [refresh, setRefresh] = useState(0)
+    const [traders, setTraders] = useState([]);
+    const [viewTrader, setViewTrader] = useState({
+        modal: false,
+        id: null,
+        email: null,
+        name: null,
+        status: null,
+        totalCash: null
+    })
+
     return (
         <div style={{backgroundColor: '#F2F2F2'}}>
-            <Navbar />
+            <AdminContext.Provider value={{adminDashboard, setAdminDashboard, refresh, setRefresh, traders, setTraders, viewTrader, setViewTrader}}>
+                <Navbar />
+                <Header />
+                {adminDashboard ? <Traders /> : null }
+                <TraderDetails />
+            </AdminContext.Provider>
         </div>
     )
 }
