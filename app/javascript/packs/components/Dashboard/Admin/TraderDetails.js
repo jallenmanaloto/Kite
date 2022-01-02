d5420704c2c7f574f732ed32af4a9f521fe7e81f
jallenmanaloto <jallen.manaloto@gmail.com>
2022-01-03 00:31:10 +0800

    Add modal to view specific trader

---
+++ app/javascript/packs/components/Dashboard/Admin/TraderDetails.js
@@ -0,0 +1,78 @@
+import React, { useContext, useState } from 'react'
+import AdminContext from '../../Contexts/AdminContext'
+import axios from 'axios'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import Modal from 'react-bootstrap/Modal'
+
+const TraderDetails = () => {
+    const { refresh, setRefresh, viewTrader, setViewTrader } = useContext(AdminContext)
+    const [traderName, setTraderName] = useState('')
+    const axios = require('axios')
+
+    const handleClose = () => {
+        setViewTrader({ modal: false })
+    }
+
+    const handleName = (e) => {
+        setTraderName(e.target.value)
+    }
+
+    const isApproved = (stat) => {
+        if (stat === 'true') {
+            return true
+        }
+        else {
+            return false
+        }
+    }
+
+    const saveDetail = () => {
+        axios({
+            method: 'patch',
+            url: 'http://localhost:3000/api/v1/users/10/traders/6/edit_trader',
+            data: {
+                name: traderName
+            }
+        })
+        .then((res) => {
+            setRefresh(refresh + 1)
+            handleClose()
+        })
+    }
+
+    return (
+        <div>
+            <Modal show={viewTrader.modal} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Trader Details</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form.Group className="trader-name">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control placeholder="name" onChange={handleName} defaultValue={viewTrader.name}/>
+                    </Form.Group>
+                    <Form.Group className="trader-email">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control placeholder={viewTrader.email} disabled/>
+                    </Form.Group>
+                    <Form.Group className="trader-status">
+                        <Form.Label>Account Status</Form.Label>
+                        <Form.Control placeholder={isApproved(viewTrader.status) ? 'Active' : 'Pending' } disabled/>
+                    </Form.Group>
+                    <Form.Group className="trader-cash">
+                        <Form.Label>Total Cash</Form.Label>
+                        <Form.Control placeholder={viewTrader.totalCash} disabled/>
+                    </Form.Group>
+                   
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="primary" onClick={saveDetail}>Save changes</Button>
+                    <Button variant="secondary" onClick={handleClose}>Cancel</Button>
+                </Modal.Footer>
+            </Modal>
+        </div>
+    )
+}
+
+export default TraderDetails
