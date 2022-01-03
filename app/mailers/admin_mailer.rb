b787348570df547e28d8c0d68566a8d4f69fdfe0
jallenmanaloto <jallen.manaloto@gmail.com>
2022-01-03 22:27:25 +0800

    Edited mail from, to the domain of project

---
+++ app/mailers/admin_mailer.rb
@@ -0,0 +1,13 @@
+class UserMailer < ApplicationMailer
+    default template_path: 'user_mailer'
+
+    def approve_account(user)
+        email = user.email.to_s
+        token = user.confirmation_token
+        mail(
+            from: "support@kite.com",
+            to: email, 
+            subject: "Pending Account",
+        )
+    end
+end
