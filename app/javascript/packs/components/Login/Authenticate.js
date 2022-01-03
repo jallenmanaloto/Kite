68ccb9e027afdd48ab7af6452a1a0b31dcbe683a
jallenmanaloto <jallen.manaloto@gmail.com>
2022-01-03 22:25:51 +0800

    Removed components and mount to main app

--- app/javascript/packs/components/Login/Authenticate.js
+++ app/javascript/packs/components/Login/Authenticate.js
@@ -1,19 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Auth from '../Contexts/Auth'
 import Login from './Login'
 import Register from './Register'
 import AdminLogin from './AdminLogin'
+import Admin from '../Dashboard/Admin/Admin'
 
 const Authenticate = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/admin-login" element={<AdminLogin />} />
-            </Routes>
-        </Router>
+    const [auth, setAuth] = useState(false)
 
+    return (
+        <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/admin-login" element={<AdminLogin />} />
+        </Routes>
     )
 }
 
