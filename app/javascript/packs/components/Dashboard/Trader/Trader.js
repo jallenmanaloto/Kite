ac434161f7c4c657857f9bac77859d5e461f6c7d
jallenmanaloto <jallen.manaloto@gmail.com>
2022-01-04 13:29:13 +0800

    Add Trader Dashboard

---
+++ app/javascript/packs/components/Dashboard/Trader/Trader.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Sidebar from './Sidebar'
+import Header from './Header'
+
+const Trader = () => {
+    return (
+        <div>
+            <Sidebar />
+
+        </div>
+    )
+}
+
+export default Trader
