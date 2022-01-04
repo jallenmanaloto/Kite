8c4f0f2e0bfba1692b14ad322014f8318daa714c
jallenmanaloto <jallen.manaloto@gmail.com>
2022-01-04 21:38:05 +0800

    Add context to get data on specific stock

--- app/javascript/packs/components/Page.js
+++ app/javascript/packs/components/Page.js
@@ -4,10 +4,13 @@ import Auth from './Contexts/Auth'
 
 const Page = () => {
     const [currentUser, setCurrentUser] = useState({})
+    const [searchStock, setSearchStock] = useState([])
+    const [searchInput, setSearchInput] = useState('')
+    const [searchShow, setSearchShow] = useState(false)
     
     return (
         <div>
-            <Auth.Provider value={{ currentUser, setCurrentUser }}>
+            <Auth.Provider value={{ currentUser, setCurrentUser, searchInput, setSearchInput, searchShow, setSearchShow, searchStock, setSearchStock }}>
                 <App />
             </Auth.Provider>
         </div>
