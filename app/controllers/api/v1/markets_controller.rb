6786e84c1aa67843d34689db2b3e7a0a23a0cd4e
jallenmanaloto <jallen.manaloto@gmail.com>
2022-01-04 21:37:28 +0800

    Add IEX client to index

--- app/controllers/api/v1/markets_controller.rb
+++ app/controllers/api/v1/markets_controller.rb
@@ -4,12 +4,15 @@ module Api
             skip_before_action :verify_authenticity_token
             def search
                 company = Market.where('LOWER(name) LIKE ?', "%#{params[:search].downcase}%")
-
-                
                 render json: {stock: company}
             end
 
             def index
+                client = IEX::Api::Client.new(
+                    publishable_token: 'pk_a020ad363b0b447185bf2db1cfd11154',
+                    secret_token: 'sk_4e277e5f5d884ae987d56fca67577a68',
+                    endpoint: 'https://cloud.iexapis.com/v1'
+                )
                 market = client.stock_market_list(:mostactive) 
 
                 render json: {mostactive: market}
