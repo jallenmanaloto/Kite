b100f7a02ae01ae1df53307bbe01c4cbb3973302
jallenmanaloto <jallen.manaloto@gmail.com>
2022-01-04 21:39:15 +0800

    Add contexts to show search result component

--- app/javascript/packs/components/Dashboard/Trader/Trader.js
+++ app/javascript/packs/components/Dashboard/Trader/Trader.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import Auth from '../../Contexts/Auth'
 import Sidebar from './Sidebar'
 import Header from './Header'
+import SearchHeader from './SearchHeader'
 
 const Trader = () => {
+
+    const { searchInput, searchShow, setSearchShow } = useContext(Auth)
     return (
         <div style={{ backgroundColor: '#eaeff3' }}>
             <Sidebar />
             <Header />
+            {searchShow ? <SearchHeader /> : null}
         </div>
     )
 }
