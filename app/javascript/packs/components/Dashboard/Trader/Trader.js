a16570cbda1c86d4faeafc26da676ec29a338c7c
jallenmanaloto <jallen.manaloto@gmail.com>
2022-01-05 14:40:29 +0800

    Configure to switch components

--- app/javascript/packs/components/Dashboard/Trader/Trader.js
+++ app/javascript/packs/components/Dashboard/Trader/Trader.js
@@ -2,16 +2,19 @@ import React, { useContext } from 'react'
 import Auth from '../../Contexts/Auth'
 import Sidebar from './Sidebar'
 import Header from './Header'
+import Wallet from './Wallet'
 import SearchHeader from './SearchHeader'
 
 const Trader = () => {
 
-    const { searchInput, searchShow, setSearchShow } = useContext(Auth)
+    const { searchInput, searchShow, setSearchShow, traderMain } = useContext(Auth)
+    
     return (
         <div style={{ backgroundColor: '#eaeff3' }}>
             <Sidebar />
             <Header />
             {searchShow ? <SearchHeader /> : null}
+            {traderMain.main === 'wallet' ? <Wallet /> : null }
         </div>
     )
 }
