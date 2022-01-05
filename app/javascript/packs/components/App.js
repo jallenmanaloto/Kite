5f10ae835b52e38bd3e52ccb12a1c345e5e0ef55
jallenmanaloto <jallen.manaloto@gmail.com>
2022-01-05 14:39:45 +0800

    Added context to switch which component to show

--- app/javascript/packs/components/App.js
+++ app/javascript/packs/components/App.js
@@ -4,20 +4,21 @@ import AdminLogin from './Login/AdminLogin'
 import Admin from './Dashboard/Admin/Admin'
 import Login from './Login/Login'
 import Register from './Login/Register'
+import Trader from '../components/Dashboard/Trader/Trader'
 import Navbar from './Dashboard/Admin/Navbar'
 
 const App = () => {
     return (
         <div>
-            <Router>
+            {/* <Router>
                 <Routes>
                     <Route path="/" element={<Login  />} />
                     <Route path="/admin" element={<AdminLogin />} />
                     <Route path="/register" element={<Register />} />
                     <Route path="/admin/dashboard" element={<Admin />} />
-                    {/* <Route path="/navbar-90982w" element={<Navbar />} /> */}
                 </Routes>
-            </Router>
+            </Router> */}
+            <Trader />
         </div>
     )
 }
