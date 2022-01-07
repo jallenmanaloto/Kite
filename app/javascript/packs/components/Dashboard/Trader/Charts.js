8f02af2c7df267ab634b3cc222251123e0ee5e85
jallenmanaloto <jallen.manaloto@gmail.com>
2022-01-07 21:43:43 +0800

    Created Charts component to hold charts

---
+++ app/javascript/packs/components/Dashboard/Trader/Charts.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import Chart from 'chart.js/auto'
+import { Line } from 'react-chartjs-2'
+import { Options } from 'styled-icons/evaicons-solid'
+
+const Charts = ({symbol}) => {
+    const [chartData, setChartData] = useState([])
+    const [chartDates, setChartDates] = useState([])
+    const [chartClose, setChartClose] = useState([])
+
+    useEffect(() => {
+        axios({
+            method: 'post',
+            url: 'http://localhost:3000/api/v1/markets/specific_company',
+            data: {
+                symbol: 'AAPL'
+            }
+        })
+        .then((res) => {
+            setChartData(res.data.charts)
+        })
+        .catch(err => console.log(err))
+    }, [])
+
+    useEffect(() => {
+        let dates = chartData.map(dates => dates.date)
+        let closes = chartData.map(closes => closes.close)
+
+        setChartDates(dates)
+        setChartClose(closes)
+    }, [chartData])
+    
+
+    return (
+        <div>
+            <Line 
+                data={{
+                    labels: chartDates,
+                    datasets:[{
+                        label: 'Last close',
+                        data: chartClose,
+                        backgroundColor: '#1F8C76',
+                        borderColor: '#1F8C76',
+                        fill: true,
+                    }]
+                }}
+                height={400}
+                width={600}
+                options={{
+                    responsive: true,
+                    maintainAspectRatio: false,
+                    scales:{
+                       x: {
+                           ticks: {
+                               display: false
+                           }
+                       }
+                    }
+                }}
+            />
+        </div>
+    )
+}
+
+export default Charts
