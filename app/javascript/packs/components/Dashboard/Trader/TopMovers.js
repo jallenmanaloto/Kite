eff1208847d8a4697b1d51713c3843f5a4cea171
jallenmanaloto <jallen.manaloto@gmail.com>
2022-01-08 18:14:16 +0800

    Add Top movers

--- app/javascript/packs/components/Dashboard/Trader/TopMovers.js
+++ app/javascript/packs/components/Dashboard/Trader/TopMovers.js
@@ -1,10 +1,103 @@
 import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import axios from 'axios'
+
+const MoversContainer = styled.div`
+    margin: 0 2em;
+    display: flex;
+    flex-direction: column;
+`
+const MoversChange = styled.h3`
+    position: absolute;
+    left: 20em;
+    font-size: 0.85rem;
+    font-family: 'Roboto', sans-serif;
+    color: #3E3D3D;
+    margin: 0;
+`
+const MoversCompany = styled.h3`
+    font-size: 1.1rem;
+    font-family: 'Roboto', sans-serif;
+    color: #3E3D3D;
+    width: 60%;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+    margin: 0;
+`
+const MoversItem = styled.div`
+    display: flex;
+    position: relative;
+    width: 80%;
+    justify-content: space-between;
+    margin-top: 1.1rem;
+`
+const MoversName = styled.h3`
+    font-size: 0.9rem;
+    font-family: 'Roboto', sans-serif;
+    color: lightgray;
+    margin: 0;
+`
+const Title = styled.h2`
+    font-size: 1rem;
+    font-family: 'Roboto', sans-serif;
+    font-weight: bold;
+    color: #3E3D3D;
+    margin-top: 1rem;
+    margin-left: 3rem;
+    margin: 1rem 0 0 2rem;
+`
+const Wrapper = styled.div`
+    position: absolute;
+    bottom: 2em;
+    right: 3em;
+    background-color: white;
+    border: 1px solid white;
+    border-radius: 7px;
+    box-shadow: 1px 1px 5px 0px rgba(0,0,0,0.23);
+    height: 48%;
+    width: 23%;
+`
 
 const TopMovers = () => {
-    return (
-        <div>
 
-        </div>
+    const [active, setActive] = useState([])
+    useEffect(() => {
+        axios({
+            method: 'get',
+            url: 'http://localhost:3000/index'
+        })
+            .then((res) => {
+                setActive(res.data.mostactive.reverse())
+            })
+            .catch(err => console.log(err))
+    }, [])
+
+    const handleChangeColor = (string) => {
+        if (string.includes('+')) {
+            return true;
+        }
+    }
+
+    const movers = active.map((company) => {
+        return (
+            <MoversItem key={company.symbol}>
+                <div style={{ display: 'flex', alignItems: 'center', width: '90%' }}>
+                    <MoversCompany>{company.company_name}</MoversCompany>
+                    <MoversName>{company.symbol}</MoversName>
+                </div>
+                <MoversChange style={{ color: `${handleChangeColor(company.change_percent_s) ? '#1F8C76' : 'red'}` }}>{company.change_percent_s}</MoversChange>
+            </MoversItem>
+        )
+    })
+
+    return (
+        <Wrapper>
+            <Title>Top Movers</Title>
+            <MoversContainer className="movers-container">
+                {movers}
+            </MoversContainer>
+        </Wrapper>
     )
 }
 
