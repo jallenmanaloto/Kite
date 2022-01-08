2702e5b72435f40f81166fd2bd946a4d71cffe58
jallenmanaloto <jallen.manaloto@gmail.com>
2022-01-08 16:27:25 +0800

    Add Charts to dashboard

--- app/javascript/packs/components/Dashboard/Trader/Charts.js
+++ app/javascript/packs/components/Dashboard/Trader/Charts.js
@@ -1,9 +1,28 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import styled from 'styled-components'
 import Chart from 'chart.js/auto'
 import { Line } from 'react-chartjs-2'
 import { Options } from 'styled-icons/evaicons-solid'
 
+const ChartHead = styled.h2`
+    font-size: 1.6rem;
+    font-family: 'Roboto', sans-serif;
+    color: #3E3D3D;
+    margin-left: 2rem;
+    margin-top: 2rem;
+`
+const Wrapper = styled.div`
+    position: fixed;
+    bottom: 2em;
+    left: 15vw;
+    height: 45%;
+    width: 60%;
+    background-color: white;
+    border: 1px solid white;
+    border-radius: 7px;
+    box-shadow: 1px 1px 5px 0px rgba(0,0,0,0.23);
+`
 const Charts = () => {
     const [chartData, setChartData] = useState([])
     const [chartDates, setChartDates] = useState([])
@@ -17,10 +36,10 @@ const Charts = () => {
                 symbol: 'AAPL'
             }
         })
-        .then((res) => {
-            setChartData(res.data.charts)
-        })
-        .catch(err => console.log(err))
+            .then((res) => {
+                setChartData(res.data.charts)
+            })
+            .catch(err => console.log(err))
     }, [])
 
     useEffect(() => {
@@ -30,36 +49,37 @@ const Charts = () => {
         setChartDates(dates)
         setChartClose(closes)
     }, [chartData])
-    
+
 
     return (
-        <div>
-            <Line 
+        <Wrapper>
+            <ChartHead>Apple Inc</ChartHead>
+            <Line
                 data={{
                     labels: chartDates,
-                    datasets:[{
+                    datasets: [{
                         label: 'Last close',
                         data: chartClose,
                         backgroundColor: '#1F8C76',
                         borderColor: '#1F8C76',
-                        fill: true,
+                        fill: false,
                     }]
                 }}
-                height={400}
-                width={600}
+                height={170}
+                width={550}
                 options={{
                     responsive: true,
-                    maintainAspectRatio: false,
-                    scales:{
-                       x: {
-                           ticks: {
-                               display: false
-                           }
-                       }
+                    maintainAspectRatio: true,
+                    scales: {
+                        x: {
+                            ticks: {
+                                display: false
+                            }
+                        }
                     }
                 }}
             />
-        </div>
+        </Wrapper>
     )
 }
 
